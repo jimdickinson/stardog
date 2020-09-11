23e81b75b5e6b7fd8d68d214049538d1c2e83402
Eric Borczuk <eric.borczuk@datastax.com>
2020-09-11 18:29:48 -0400

    Made a static index page (#2)

--- stardog_server.py
+++ stardog_server.py
@@ -1,20 +1,43 @@
+import os
 import cherrypy
-from stardog.endpoints.query_frontend import QueryEndpoint
+from cherrypy.lib.static import serve_file
 from stardog.endpoints.execute_query import ExecuteQueryEndpoint
 from stardog.endpoints.new_document import NewDocumentEndpoint
+from jinja2 import Environment, PackageLoader, select_autoescape
 
+env = Environment(
+    loader=PackageLoader('stardog.endpoints', 'resources'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 class StardogServer:
-    pass
+    @cherrypy.expose
+    def static(self, name):
+        if name.endswith('css'):
+            return serve_file(os.path.join(CURRENT_DIR, 'stardog', 'endpoints', 'resources', 'static', name), content_type='text/css')
+        if name.endswith('svg'):
+            return serve_file(os.path.join(CURRENT_DIR, 'stardog', 'endpoints', 'resources', 'static', name), content_type='image/svg+xml')
+
+    @cherrypy.expose
+    def query(self, name):
+        if name == 'index.html':
+            template = env.get_template(name)
+            return template.render({'pods': [{'name': 'Pod1'}, {'name': 'Pod2'}, {'name': 'Pod3'}]})
+
+        raise cherrypy.HTTPError(404, message="Resource Not Found")
+
+    @cherrypy.expose
+    def item(self, name):
+        if name.endswith('js'):
+            return serve_file(os.path.join(CURRENT_DIR, 'stardog', 'endpoints', 'resources', name), content_type='text/javascript')
+        template = env.get_template('item_explorer.html')
+        return template.render({'name': name})
 
 class Api:
     pass
 
 CHERRY_TREE_CONFIG = {
-    '/query': {
-        'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-        'tools.response_headers.on': True,
-        'tools.response_headers.headers': [('Content-Type', 'text/html')],
-    },
     '/api/executeQuery': {
         'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
         'tools.response_headers.on': True,
@@ -33,11 +56,10 @@ def setup_cherry_tree(port=8080):
     cherrypy.config.update({
         'server.socket_port': port,
         'environment': 'production',
-        'log.screen': False,
-        'show_tracebacks': False,
+        'log.screen': True,
+        'show_tracebacks': True,
     })
     service = StardogServer()
-    service.query = QueryEndpoint()
     service.api = Api()
     service.api.executeQuery = ExecuteQueryEndpoint()
     service.api.newDocument = NewDocumentEndpoint()
