8f505529f42c4c9ad68058ba91766f62fa5fbd2d
Jim Dickinson <jim.dickinson@datastax.com>
2020-09-12 00:03:55 -0400

    Use fire in the fetcher, better mkdir, more pythonic

--- stardog_fetcher.py
+++ stardog_fetcher.py
@@ -1,37 +1,41 @@
 import os
+import pathlib
 import subprocess
 import time
+import fire
 
-context_str = """kube1
-kube2
-kube3"""
+def fetch_contexts(*contexts):
+    '''
+    loop through the contexts passed in as varargs, and extract data from kube for each
+    '''
 
-contexts = context_str.split('\n')
+    for kubecontext in contexts:
+        print("working on {}".format(kubecontext))
+        pathlib.Path("/tmp/{}".format(kubecontext)).mkdir(parents=True, exist_ok=True)
 
-for kubecontext in contexts:
-    print("working on {}".format(kubecontext))
-    os.mkdir("/tmp/{}".format(kubecontext))
+        filename = "/tmp/{}/pods.json".format(kubecontext)
+        print("writing to {}".format(filename))
+        subprocess.run(args=[
+            "bash", "-c",
+            "kubectl --context={} get pods -A -o json > {}".format(kubecontext, filename)
+            ])
+        time.sleep(2)
 
-    filename = "/tmp/{}/pods.json".format(kubecontext)
-    print("writing to {}".format(filename))
-    subprocess.run(args=[
-        "bash", "-c",
-        "kubectl --context={} get pods -A -o json > {}".format(kubecontext, filename)
-        ])
-    time.sleep(2)
+        filename = "/tmp/{}/namespaces.json".format(kubecontext)
+        print("writing to {}".format(filename))
+        subprocess.run(args=[
+            "bash", "-c",
+            "kubectl --context={} get namespaces -A -o json > {}".format(kubecontext, filename)
+            ])
+        time.sleep(2)    
 
-    filename = "/tmp/{}/namespaces.json".format(kubecontext)
-    print("writing to {}".format(filename))
-    subprocess.run(args=[
-        "bash", "-c",
-        "kubectl --context={} get namespaces -A -o json > {}".format(kubecontext, filename)
-        ])
-    time.sleep(2)    
+        filename = "/tmp/{}/deployments.json".format(kubecontext)
+        print("writing to {}".format(filename))
+        subprocess.run(args=[
+            "bash", "-c",
+            "kubectl --context={} get deployments -A -o json > {}".format(kubecontext, filename)
+            ])
+        time.sleep(2)
 
-    filename = "/tmp/{}/deployments.json".format(kubecontext)
-    print("writing to {}".format(filename))
-    subprocess.run(args=[
-        "bash", "-c",
-        "kubectl --context={} get deployments -A -o json > {}".format(kubecontext, filename)
-        ])
-    time.sleep(2)
+if __name__ == '__main__':
+    fire.Fire(fetch_contexts)
