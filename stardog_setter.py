cae13e88c5962d28d6e4376ecac67b7edaed8e67
Eric Borczuk <eric.borczuk@datastax.com>
2020-09-12 02:20:31 -0400

    Add better fail messaging at end of run

--- stardog_setter.py
+++ stardog_setter.py
@@ -6,6 +6,7 @@ from stardog_loader import process as loader_process
 
 
 INVALID_CHAR_REGEX = r"('|,|\.|\[|\]|\*)"
+FAILED = []
 
 def delete_then_put(item, ctx, ns, kind, astra_proxy_url):
     # Delete it if it's already there
@@ -17,15 +18,17 @@ def delete_then_put(item, ctx, ns, kind, astra_proxy_url):
         if resp.status_code != 204 and not first_time_putting:
             delete_fail = True
             print(f'Delete failed: {resp.text} {resp.status_code}')
+            return False
 
-        if not delete_fail:
-            resp = requests.put(astra_proxy_url + f'/api/rest/v2/namespaces/stardog/collections/{kind}/{name}', json=item)
-            if resp.status_code != 200:
-                print(f'Put failed: {resp.text} {resp.status_code} {item}')
+        resp = requests.put(astra_proxy_url + f'/api/rest/v2/namespaces/stardog/collections/{kind}/{name}', json=item)
+        if resp.status_code != 200:
+            print(f'Put failed: {resp.text} {resp.status_code} {item}')
+            return False
     except KeyError as e:
         raise RuntimeError(f'The key {e} was not found in {item}')
     
     print(f'Successfully put the data for {kind[:-1]} {name}')
+    return True
 
 def sub_bad_chars(data):
     if isinstance(data, dict):
@@ -35,12 +38,16 @@ def sub_bad_chars(data):
 
 KINDS = ['namespaces', 'pods', 'deployments']
 def process(stardog_data_dir, astra_proxy_url):
+    fails = []
     for directory in os.listdir(stardog_data_dir):
         if directory.startswith('.'):
             continue
         for kind in KINDS:
             for (ctx, namespace, data) in loader_process(os.path.join(stardog_data_dir, directory), kind):
-                delete_then_put(sub_bad_chars(data), ctx, namespace, kind, astra_proxy_url)
+                success = delete_then_put(sub_bad_chars(data), ctx, namespace, kind, astra_proxy_url)
+                if not success:
+                    fails.append((ctx, namespace, kind, item['metadata']['name']))
+    print(f"The following {len(fails)} items failed: {fails}")
 
 if __name__ == '__main__':
     fire.Fire(process)
